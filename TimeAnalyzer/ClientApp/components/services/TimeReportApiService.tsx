b4a2faa74f62054ac0dcd8788fc1c52dafc355eb
Andrey <rudenkoandreykh@gmail.com>
2018-12-14 18:32:05 +0200

    merge

--- TimeAnalyzer/ClientApp/components/services/TimeReportApiService.tsx
+++ TimeAnalyzer/ClientApp/components/services/TimeReportApiService.tsx
@@ -4,21 +4,21 @@ import TimeConverterService from './TimeConverterService';
 import axios from "axios"
 
 export default class TimeReportApiService extends React.Component<any, any>{
-    
-    authorizedApi:AuthorizeHttpRequestService;
+
+    authorizedApi: AuthorizeHttpRequestService;
     timeReportApiService: TimeConverterService;
 
-    constructor(){
+    constructor() {
         super();
         this.authorizedApi = new AuthorizeHttpRequestService();
         this.timeReportApiService = new TimeConverterService();
     }
-    
+
     getAllUserTimeReports() {
         return this.authorizedApi.authorizedGet('/api/TimeReport/GetUserTimeReports', null)
-        .then((response: any) => {
-            return Promise.resolve(response);
-        });
+            .then((response: any) => {
+                return Promise.resolve(response);
+            });
     }
 
     getDayUserTimeReports(date: any) {
@@ -29,8 +29,10 @@ export default class TimeReportApiService extends React.Component<any, any>{
     getUserTimeReportsInInterval(startDate: any, endDate: any) {
         var jsonStartDate = this.timeReportApiService.toServerFormatDate(startDate);
         var jsonEndDate = this.timeReportApiService.toServerFormatDate(endDate);
-        return this.authorizedApi.authorizedGet('/api/TimeReport/GetTimeReportsInInterval', [jsonStartDate, jsonEndDate]);
-    addTimeReport( Date: any, Duration: any, ActivityId: any) {
+     return this.authorizedApi.authorizedGet('/api/TimeReport/GetTimeReportsInInterval', [jsonStartDate, jsonEndDate]);
+    }
+
+    addTimeReport(Date: any, Duration: any, ActivityId: any) {
         console.log("date syka",Date)
         var jsonDate = this.timeReportApiService.toServerFormatDate(Date);
         return axios.post(`/api/TimeReport/AddTimeReport`, {
